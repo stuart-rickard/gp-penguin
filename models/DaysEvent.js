0ff98152287dc15d1748ddf7a8835c31823f3bd0
jochun <jkchun@protonmail.com>
2022-02-06 10:42:21 -1000

    updating models

--- models/DaysEvent.js
+++ models/DaysEvent.js
@@ -1,7 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class DaysEvents extends Model { }
+class DaysEvent extends Model { }
 
 
-module.exports = DaysEvents;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class DaysEvent extends Model { }
+
+DaysEvent.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        event_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'event',
+                key: 'id'
+            }
+        },
+        day_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'day',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'daysEvent'
+    }
+);
+
+module.exports = DaysEvent;
---
+++ models/Vote.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model { }
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        DE_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'daysEvent',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+module.exports = Vote;
