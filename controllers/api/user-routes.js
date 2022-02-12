7abb1726dee077f0d31275a4c90fc102687aaed4
Chance <chancecrawford30@gmail.com>
2022-02-11 18:23:30 -0800

    added post and delete user routes and hashed user pass

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -16,7 +16,6 @@ router.get('/', (req, res) => {
 // one user based on id
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         },
@@ -27,7 +26,52 @@ router.get('/:id', (req, res) => {
             }
         ]
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        // if user doesnt exist or has been deleted
+        if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+        }
+        // send the json of that user from the table as an object
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create a new user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        // this password gets hashed for security before getting
+        // saved in the database. see User.js
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+          id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
--- models/User.js
+++ models/User.js
@@ -1,7 +1,16 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt');
 
-class User extends Model { }
+class User extends Model { 
+    checkPassword(loginPw) {
+        // Using the keyword this, we can access this user's 
+        // properties, including the password, which was stored as a hashed string.
+        // it is prefered to run this functions async version on a live website
+        // but for testing purposes, we are using the sync version.
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -35,6 +44,14 @@ User.init(
 
     },
     {
+        hooks: {
+            // before user is created and added to the database, this will hash the password
+            // and then save it to the database.
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
