8682e46225412ebbb5cb1d83b27e171311edf479
stuart-rickard <stuart@bau-dev.com>
2022-02-14 18:54:09 -0800

    new page sequence

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,81 +1,89 @@
 const router = require("express").Router();
 const { Event, User, Vote } = require("../models");
-const withAuth = require('../utils/auth');
+const withAuth = require("../utils/auth");
 
 // send to event creation page, or if not authorized user,
 // send to the login page.
 router.get("/", (req, res) => {
+  res.render("login", {
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+// event entry page
+router.get("/event-entry", (req, res) => {
   Event.findAll({
     where: {
-      user_id: 3
+      // TODO capture correct user_id
+      user_id: 3,
     },
     include: [
-        {
-          model: User,
-          attributes: ['username', 'id']
-        },
-        {
-            model: Vote,
-            attributes: ['id']
-        }
-    ]
+      {
+        model: User,
+        attributes: ["username", "id"],
+      },
+      {
+        model: Vote,
+        attributes: ["id"],
+      },
+    ],
   })
-  .then(dbEventData => {
-    // serialize events to regular objects
-    let events = dbEventData.map(event => event.get({ plain: true }));
+    .then((dbEventData) => {
+      // serialize events to regular objects
+      let events = dbEventData.map((event) => event.get({ plain: true }));
 
-    // put events in desc order
-    events = events.reverse();
+      // put events in desc order
+      events = events.reverse();
 
-    res.render('home', { 
-      events,
-      loggedIn: req.session.loggedIn
-    });
-  })
-  .catch(err => {
+      res.render("event-entry", {
+        events,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
-  });
+    });
 });
 
 // vote page for an event
 router.get("/vote/:id", (req, res) => {
   Event.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
     include: [
-        {
-          model: User,
-          attributes: ['username', 'id']
-        },
-        {
-            model: Vote,
-            attributes: ['id', 'event_id', 'days']
-        }
-    ]
+      {
+        model: User,
+        attributes: ["username", "id"],
+      },
+      {
+        model: Vote,
+        attributes: ["id", "event_id", "days"],
+      },
+    ],
   })
-  .then(dbEventData => {
-    // serialize events to regular objects
-    const event = dbEventData.get({ plain: true });
+    .then((dbEventData) => {
+      // serialize events to regular objects
+      const event = dbEventData.get({ plain: true });
 
-    res.render('tally', { 
-      event
-    });
-  })
-  .catch(err => {
+      res.render("tally", {
+        event,
+      });
+    })
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
-  });
+    });
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
