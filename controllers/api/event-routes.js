2f47a242c032589177746daf719d03a8d822be47
Chance <chancecrawford30@gmail.com>
2022-02-15 18:55:44 -0800

    vote page can now post votes to events

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { User, Event, Vote } = require('../../models');
+// use sengrid to send emails to the emails on the invitee list
+const connectAndSendEmail = require("../../utils/email-connect-and-send");
 
 // all events
 router.get('/', (req, res) => {
@@ -61,7 +63,20 @@ router.post('/', (req, res) => {
         days: req.body.days,
         invite_emails: req.body.invite_emails
     })
-    .then(dbEventData => res.json(dbEventData))
+    .then(dbEventData => {
+        const event = dbEventData.get({ plain: true });
+
+        // for(let i = 0; i < event.invite_emails; i++){
+        //     connectAndSendEmail(event.invite_emails[i], 
+        //     'You have been invited to vote!',
+        //     `You have received an invitation to vote on the event: "${event.title}".
+        //      Please go to this URL to access the event vote page.
+        //      http://localhost:3001/vote/${event.id}
+        //     `
+        //     );
+        // }
+        res.json(event);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
